2dd12b4435385d73d3328b684771618e39bc9d8d
Veneta <venkireva80@gmail.com>
2022-07-25 20:08:36 +0300

    Add basic interface for Tree class

---
+++ source/tree.h
@@ -0,0 +1,41 @@
+#ifndef TREEMANAGER_TREE_H
+#define TREEMANAGER_TREE_H
+
+#include <vector>
+#include <iostream>
+
+using std::vector;
+
+/// Represents a tree
+class Tree {
+    /// Represents a node in the tree
+    struct Node {
+        /// The integer saved in the node
+        int value;
+
+        /// Children nodes
+        vector<Node*> children;
+
+        /// Default constructor
+        Node() = default;
+    };
+
+    /// The root of the tree
+    Node* root;
+
+    void sortChildren(Node* currentRoot);
+
+public:
+    Tree();
+    Tree(Tree const& other) = delete;
+    Tree& operator=(Tree const& other) = delete;
+    ~Tree();
+
+    void read(std::istream& in);
+
+    void containsSubtree(const Tree* subtree);
+    void removeSubtree(const Tree* subtree);
+};
+
+
+#endif //TREEMANAGER_TREE_H
