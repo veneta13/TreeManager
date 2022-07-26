c0ce4be907f19f02ac221c0882cc5880952d8474
Veneta <venkireva80@gmail.com>
2022-07-26 21:54:50 +0300

    Add test trees

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,4 +3,23 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+const char* tree1 = "| 8 |\n"
+                    "| 3 2 9 |\n"
+                    "| 2 | 8 6 | 2 |\n"
+                    "| 8 6 | 11 | 12 | 8 5 |\n"
+                    "| 11 | 4 3 | | | 11 | 6 |\n"
+                    "| | | | | | ";
+
+const char* tree2 = "| 2 |\n"
+                    "| 8 6 |\n"
+                    "| 11 | |\n"
+                    "| |";
+
+const char* tree3 = "| 8 |\n"
+                    "| 3 12 9 |\n"
+                    "| 7 | | 2 |\n"
+                    "| | 8 5 |\n"
+                    "| 11 | 6 |\n"
+                    "| | |";
+
 #pragma clang diagnostic pop
